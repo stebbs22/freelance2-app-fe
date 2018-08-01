8f34bff4347c7b3953eec367a8f93ba8749721be
Steven Tebbs <sven_was_here@yahoo.com>
2018-08-01 15:38:07 -0600

    Updated styles for app.component.css and html page

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
 	moduleId: module.id,
 	selector: 'main-app',
-	template: '<h1>Freelance Bootcamp Dashboard</h1>'
+	templateUrl: './homepage.component.html'
 })
 export class HomepageComponent {}
